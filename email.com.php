e77215903b089b141fe035e32d5826e523d24738
karlody <78699937+karlody@users.noreply.github.com>
2021-02-08 01:32:26 +0700

    Add files via upload

---
+++ email.com.php
@@ -0,0 +1,10 @@
+<?php
+/*
+############################################
+##          Author : Koe Nurf             ##
+############################################
+/*
+/* PERHATIAN 'Kalo Ga Bisa Bahasa PHP Ga Usah Gonta Ganti' */
+$from= 'amoy@pencoli-sec.blogspot.com';     /* Nama Pengirim */       
+$emailku = 'diwanwrpkp@gmail.com';   /* Penerima Email */  
+?>
